a41a719253eadcb80be62fb03cb81fe2f1ede26b
Mohammed Ehab <mohammedehab93@gmail.com>
2019-04-30 14:58:29 +0400

    App with frontend

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const app = express();
 const cors = require("cors");
-
+const path = require("path");
 //Allow cross-origin
 app.use(cors());
 
@@ -14,6 +14,14 @@ app.use(
     graphiql: true
   })
 );
+
+//serve static assets if in production - DONE FOR HEROKU INTEGRATION
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("public"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
